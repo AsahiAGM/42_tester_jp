bfd15787d357cdae7622e851759425d443d18d21
AsahiAGM <git@github.com:nojishin/bsq.git>
2025-12-03 17:57:55 +0900

    printf tester beta OK

--- run.sh
+++ run.sh
@@ -5,16 +5,26 @@ echo "
                 42 tester
 ===========================================
 "
-echo "TEST_DIR=" > testenv.txt
+
+PWD=$(pwd)
 
 echo "What is the name to the directory you want to review?"
+find .. -maxdepth 1 -mindepth 1 -type d ! -name "*.*" -printf '%f\n'
+echo "--------------------------------------------"
 read DIR
 
-find ../"$DIR" || { echo "Directory not found. Exiting."; exit 1; }
-sed -i '' "s/^TEST_DIR=.*$/TEST_DIR=\"../$DIR\"/" testenv.txt
+if [ ! -d "../$DIR" ]; then
+	echo "Directory not found. Exiting."
+	exit 1
+fi
+
+#echo "TEST_DIR=" > testenv.txt
+#sed -i "s|^TEST_DIR=.*$|TEST_DIR=\"$PWD/$DIR\"|g" testenv.txt
 
 echo "choose project ? [exit -> e]"
-find 42_tester -maxdepth 1 -mindepth 1 -type d ! -name "*.*" -printf '%f\n'
+echo 
+find . -maxdepth 1 -mindepth 1 -type d ! -name "*.*" -printf '%f\n'
+echo "--------------------------------------------"
 read TESTER
 
 echo "do test on: $TESTER"
@@ -23,9 +33,21 @@ if [ "$TESTER" = "e" ]; then
     exit 0
 fi
 
-if [ ! -d "42_tester/$TESTER" ]; then
+if [ ! -d "$TESTER" ]; then
     echo "No such tester. Exiting."
     exit 1
 fi
 
-sh "42_tester/$TESTER/test.sh"
+# copy projects
+mkdir tmp
+cp -Rp ../$DIR/. tmp/
+cp -Rp $TESTER/. tmp/
+cd tmp
+
+# run test
+ls -la
+sh test.sh
+
+# exit
+cd ..
+rm -rf tmp
